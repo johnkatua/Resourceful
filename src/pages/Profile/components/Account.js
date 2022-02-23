3640dc26c1a8e3a619901295fbadeab41a04094d
john katua <johnkatua99@gmail.com>
2022-02-23 19:37:47 +0300

    Added a tooltip to enhance accessibility of user social links

--- src/pages/Profile/components/Account.js
+++ src/pages/Profile/components/Account.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { MdOutlineEmail, MdPhone } from "react-icons/md";
 import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 import { GoLocation } from 'react-icons/go';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 import { getProfileByAccount } from '../../../redux/action/Profile';
 
@@ -25,38 +26,50 @@ const Account = () => {
         const { about, email, facebook, instagram, linkedIn, location, phone, twitter, website } = profile;
         return (
           <div className="profile--data">
-            <div className='profile--data__container'>
+            <div className="profile--data__container">
               <div className="profile--data__header">
                 <h1>My Profile</h1>
                 <span>Edit</span>
               </div>
               <div className="profile--data__body">
-                <div className="profile--data__body--about">
-                  {about}
-                </div>
+                <div className="profile--data__body--about">{about}</div>
                 <div className="profile--data__body--contact">
-                  <span>
-                    <MdOutlineEmail />
-                  </span>
-                  <span>
-                    <MdPhone />
-                  </span>
-                  <span>
-                    <BsFacebook />
-                  </span>
-                  <span>
-                    <BsInstagram />
-                  </span>
-                  <span>
-                    <BsLinkedin />
-                  </span>
-                  <span>
-                    <BsTwitter />
-                  </span>
-                  {website !== 'N/A' ? <span>{website}</span> : null}
-                  <span>
-                    <GoLocation />
-                  </span>
+                  <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-bottom">{email}</Tooltip>}>
+                    <span>
+                      <MdOutlineEmail />
+                    </span>
+                  </OverlayTrigger>
+                  <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-bottom">{phone}</Tooltip>}>
+                    <span>
+                      <MdPhone />
+                    </span>
+                  </OverlayTrigger>
+                  <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-bottom">{facebook}</Tooltip>}>
+                    <span>
+                      <BsFacebook />
+                    </span>
+                  </OverlayTrigger>
+                  <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-bottom">{instagram}</Tooltip>}>
+                    <span>
+                      <BsInstagram />
+                    </span>
+                  </OverlayTrigger>
+                  <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-bottom">{linkedIn}</Tooltip>}>
+                    <span>
+                      <BsLinkedin />
+                    </span>
+                  </OverlayTrigger>
+                  <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-bottom">{twitter}</Tooltip>}>
+                    <span>
+                      <BsTwitter />
+                    </span>
+                  </OverlayTrigger>
+                  {website !== "N/A" ? <span>{website}</span> : null}
+                  <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-bottom">{location}</Tooltip>}>
+                    <span>
+                      <GoLocation />
+                    </span>
+                  </OverlayTrigger>
                 </div>
               </div>
             </div>
